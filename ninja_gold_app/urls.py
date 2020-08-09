fbb9e7214f093345344bbb11d5cc7b78448efe9a
Tom Tseng <tomwstseng@gmail.com>
2020-08-08 17:49:42 -0700

    hw

--- ninja_gold_app/urls.py
+++ ninja_gold_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('gold', views.default_view),
     path('post', views.handle_post),
     path('', views.times_table),
     path('pacman', views.pacman)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
